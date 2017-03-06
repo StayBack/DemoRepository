32fda58420649cdbab09da7ea47bf8d2e152e3fa
kongzhaozhuang <kongzhaozhuang@sina.com>
2017-03-06 10:10:28 +0800

    初始化

---
+++ Bluetooth/Bluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bluetooth
+//
+//  Created by new on 2017/3/6.
+//  Copyright © 2017年 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bluetooth/Bluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bluetooth
+//
+//  Created by new on 2017/3/6.
+//  Copyright © 2017年 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
